0eb7cac73d7f2515524e5cd44297cb045db81b72
Vicky Lau <vlau.nyc@gmail.com>
2019-01-04 17:19:18 -0500

    created selectLibrary action, added touchable component to ListItem and connected to redux, created reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux'; // allow are reducers to work together
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
+// in combineReducer, the key you choose for your section reducer is how the property will show up on the state object.
 export default combineReducers({
-  libraries: LibraryReducer
+  libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer
 });
