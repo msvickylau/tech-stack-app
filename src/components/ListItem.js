0761a8102d795afea814144259e47afb4853335d
Vicky Lau <vlau.nyc@gmail.com>
2019-01-07 16:51:20 -0500

    added styling to body text, added animation, moved compairing id lodgic inside mapStateToProps.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableWithoutFeedback } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
-  renderDescription() {
-    const { library, selectedLibraryID } = this.props;
+  componentDidUpdate() {  //called before the UI is painted
+    LayoutAnimation.spring();
+  }
 
-    if (library.item.id === selectedLibraryID) {
+  renderDescription() {
+    const { bodyStyle } = styles;
+    const { library, expanded } = this.props;
+
+    if (expanded) {
       return (
-        <Text>{library.item.description}</Text>
+        <CardSection>
+          <Text style={bodyStyle}>{library.item.description}</Text>
+        </CardSection>
       );
     }
   }
@@ -40,11 +52,19 @@ const styles = {
   titleStyle: {
     fontSize: 18,
     paddingLeft: 15
+  },
+  bodyStyle: {
+    color: 'gray',
+    paddingLeft: 15,
+    paddingRight: 15
   }
 };
 
-const mapStateToProps = state => {
-  return { selectedLibraryID: state.selectedLibraryID };
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryID === ownProps.library.item.id;
+
+  // return { selectedLibraryID: state.selectedLibraryID };
+  return { expanded };
 };
 /* 
 mapStateToProps function will be called with our application state
@@ -53,6 +73,11 @@ so we'll return selectedLibraryID and that is going to be state.selectedLibraryI
 state.selectedLibraryID was defined in reducers/index.js when we wire up the reducer
 to the application. because this property was assigned to the reducer, we now have a
 selected libraryID piece of state.
+
+we are passing ownProps as a second argument. ownProps is === this.props in the component. 
+so any props that we pass to the ListItem component will show up inside of mapStateToProps
+as ownProps. So this allows us to do some caculation of what props we want to pass to
+our component. We can create const expanded and return only if it is expanded.
 
 the last thing to do is too pass this mapStateToProps function as the first argument 
 to our connect helper
